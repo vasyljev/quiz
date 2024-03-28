19a49f3374a9e08afd37ff26b01ce35f295313af
Mykyta <mykyta.vasyljev@gmail.com>
2024-03-28 08:30:38 +0200

    Added main backgrounds

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './Welcome.css';
 
 const Welcome = () => {
-  return <div className="Welcome">Welcome</div>;
+  return (
+    <div className="Welcome">
+      <div className="text-block">
+        <h3 className="title">Title</h3>
+        <h1 className="main-text">Text</h1>
+      </div>
+      <footer>
+        <p>Vasyliev Mykyta© 2024</p>
+      </footer>
+    </div>
+  );
 };
 
 export default Welcome;
